0f7ba380f7589f7797ee28cedc5450bed001c8de
LahoucineABOULHASSAN <em.aboulhassan@gmail.com>
2021-05-09 16:11:16 +0100

    Header and Footer components added + each one style

---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+const Footer = () => {
+  return (
+    <footer className="flex flex-column">
+      <p>
+        API from{" "}
+        <a href="https://covid19api.com" target="_blank" rel="noreferrer">
+          covid19api.com
+        </a>
+      </p>
+      <p>
+        Copyright &copy; {new Date().getFullYear()} ||
+        <span>
+          <a
+            href="https://vueportfolioapp.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Lahoucine Aboulhassan
+          </a>
+        </span>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="flex flex-column">
+      <h1>C19Tracker</h1>
+      <p>Stay home, Stay safe</p>
+    </header>
+  );
+};
+
+export default Header;
