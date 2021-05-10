49b0c25d9bf332126e0bf57b1b274454ceb7e98b
LahoucineABOULHASSAN <em.aboulhassan@gmail.com>
2021-05-10 01:15:37 +0100

    coutries styling

--- src/pages/Countries.jsx
+++ src/pages/Countries.jsx
@@ -4,9 +4,9 @@ const Countries = ({ countries }) => {
   return (
     <section className="flex flex-column">
       <h2>Search to filter countries</h2>
-      <div className="boxs flex flex-row">
+      <div className="countries flex flex-column">
         {countries.map((country) => {
-          return <Country key={country.id} country={country} />;
+          return <Country key={country.ID} country={country} />;
         })}
       </div>
     </section>
--- src/pages/countries/Country.jsx
+++ src/pages/countries/Country.jsx
@@ -1,8 +1,39 @@
 const Country = ({ country }) => {
   return (
-    <div className="flex flex-row">
-      <h3> {country.Country} </h3>
-    </div>
+    <table className="country">
+      <thead>
+        <h3> {country.Country} </h3>
+      </thead>
+      <tbody>
+        <tr className="flex flex-row">
+          <td className="flex">Recovers</td>
+          <td className="flex">Cases</td>
+          <td className="flex">Deaths</td>
+        </tr>
+        <tr className="flex flex-row">
+          <td className="flex">
+            <span>New:</span> <span>{country.NewConfirmed}</span>{" "}
+          </td>
+          <td className="flex">
+            <span>New:</span> <span>{country.NewRecovered}</span>{" "}
+          </td>
+          <td className="flex">
+            <span>New:</span> <span>{country.NewDeaths}</span>{" "}
+          </td>
+        </tr>
+        <tr className="flex flex-row">
+          <td className="flex">
+            <span>Global:</span> <span>{country.TotalConfirmed}</span>{" "}
+          </td>
+          <td className="flex">
+            <span>Global:</span> <span>{country.TotalRecovered}</span>{" "}
+          </td>
+          <td className="flex">
+            <span>Global:</span> <span>{country.TotalDeaths}</span>{" "}
+          </td>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
